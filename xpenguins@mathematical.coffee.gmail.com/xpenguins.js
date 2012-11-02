04f4722717b83cdec8b3de1b4de7c4222448bc96
mathematicalcoffee <mathematical.coffee@gmail.com>
2012-11-03 00:56:24 +1000

    bugfix - when running in non-desktop mode and entering the overview, toons should hide

--- xpenguins@mathematical.coffee.gmail.com/xpenguins.js
+++ xpenguins@mathematical.coffee.gmail.com/xpenguins.js
@@ -396,14 +396,13 @@ XPenguinsLoop.prototype = {
                 'notify::minimized', Lang.bind(this, this._onXPenguinsWindowMinimized));
             this.connectAndTrack(this._XPenguinsWindow, this._XPenguinsWindow.meta_window,
                 'workspace-changed', Lang.bind(this, this._onXPenguinsWindowWorkspaceChanged));
-        } else {
-        /* pause on overview show, if we're on the desktop */
-            this.connectAndTrack(this, Main.overview, 'showing',
-                Lang.bind(this, function () {
-                    this.pause(true, Main.overview, 'hiding');
-                })
-            );
         }
+        /* pause on overview show, if we're on the desktop */
+        this.connectAndTrack(this, Main.overview, 'showing',
+            Lang.bind(this, function () {
+                this.pause(true, Main.overview, 'hiding');
+            })
+        );
         WindowListener.WindowListener.prototype._connectSignals.apply(this, arguments);
     },
 
